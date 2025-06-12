0447675ccc95f470fad05924641ae394179aa6df
Tecvan-fe <tecvan.fe@qq.com>
2025-06-12 18:28:19 +0800

    chore: docs for coze design

---
+++ packages/components/docs/README.md
@@ -0,0 +1,14 @@
+# @coze-arch/coze-design-doc
+coze-design的文档站: https://coze-design.gf.bytedance.net/
+
+## 开发
+
+```bash
+rushx dev
+```
+
+## 构建
+
+```bash
+rushx build
+```
---
+++ packages/utils/pkg-root-webpack-plugin/vitest.config.ts
@@ -0,0 +1,16 @@
+// FIXME: Unable to resolve path to module 'vitest/config'
+import { defaultExclude } from 'vitest/config';
+import { defineConfig } from '@coze-arch/vitest-config';
+
+export default defineConfig({
+  preset: 'node',
+  dirname: __dirname,
+  test: {
+    globals: true,
+    mockReset: false,
+    coverage: {
+      provider: 'v8',
+      exclude: ['.eslintrc.js', 'lib', ...defaultExclude],
+    },
+  },
+});
