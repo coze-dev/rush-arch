2c7ad54b56e222976c5a0fcef9aeb6f009b72ff5
sanmaopep <maoyiwei.ebay@bytedance.com>
2025-08-28 13:31:22 +0800

    feat: remove ts dependencies

--- packages/text-editor/code-language-typescript/src/as.ts
+++ packages/text-editor/code-language-typescript/src/as.ts
@@ -2,7 +2,95 @@
 //  SPDX-License-Identifier: MIT
 
 import { CompletionItemKind } from 'vscode-languageserver-types';
-import { ScriptElementKind } from 'typescript';
+
+/**
+ * Copied from TypeScript 5.8.2
+ */
+enum ScriptElementKind {
+  unknown = '',
+  warning = 'warning',
+  /** predefined type (void) or keyword (class) */
+  keyword = 'keyword',
+  /** top level script node */
+  scriptElement = 'script',
+  /** module foo {} */
+  moduleElement = 'module',
+  /** class X {} */
+  classElement = 'class',
+  /** var x = class X {} */
+  localClassElement = 'local class',
+  /** interface Y {} */
+  interfaceElement = 'interface',
+  /** type T = ... */
+  typeElement = 'type',
+  /** enum E */
+  enumElement = 'enum',
+  enumMemberElement = 'enum member',
+  /**
+   * Inside module and script only
+   * const v = ..
+   */
+  variableElement = 'var',
+  /** Inside function */
+  localVariableElement = 'local var',
+  /** using foo = ... */
+  variableUsingElement = 'using',
+  /** await using foo = ... */
+  variableAwaitUsingElement = 'await using',
+  /**
+   * Inside module and script only
+   * function f() { }
+   */
+  functionElement = 'function',
+  /** Inside function */
+  localFunctionElement = 'local function',
+  /** class X { [public|private]* foo() {} } */
+  memberFunctionElement = 'method',
+  /** class X { [public|private]* [get|set] foo:number; } */
+  memberGetAccessorElement = 'getter',
+  memberSetAccessorElement = 'setter',
+  /**
+   * class X { [public|private]* foo:number; }
+   * interface Y { foo:number; }
+   */
+  memberVariableElement = 'property',
+  /** class X { [public|private]* accessor foo: number; } */
+  memberAccessorVariableElement = 'accessor',
+  /**
+   * class X { constructor() { } }
+   * class X { static { } }
+   */
+  constructorImplementationElement = 'constructor',
+  /** interface Y { ():number; } */
+  callSignatureElement = 'call',
+  /** interface Y { []:number; } */
+  indexSignatureElement = 'index',
+  /** interface Y { new():Y; } */
+  constructSignatureElement = 'construct',
+  /** function foo(*Y*: string) */
+  parameterElement = 'parameter',
+  typeParameterElement = 'type parameter',
+  primitiveType = 'primitive type',
+  label = 'label',
+  alias = 'alias',
+  constElement = 'const',
+  letElement = 'let',
+  directory = 'directory',
+  externalModuleName = 'external module name',
+  /**
+   * <JsxTagName attribute1 attribute2={0} />
+   * @deprecated
+   */
+  jsxAttribute = 'JSX attribute',
+  /** String literal */
+  string = 'string',
+  /** Jsdoc @link: in `{@link C link text}`, the before and after text "{@link " and "}" */
+  link = 'link',
+  /** Jsdoc @link: in `{@link C link text}`, the entity name "C" */
+  linkName = 'link name',
+  /** Jsdoc @link: in `{@link C link text}`, the link text "link text" */
+  linkText = 'link text',
+}
 
 /*
  * Copyright (C) 2018 TypeFox and others.
