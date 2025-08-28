b1ea17ccac72e336706e4a1a557db611dd64df62
huahua-lina <446472854@qq.com>
2025-08-28 14:30:18 +0800

    chore: update shiki to fine-grained bundle

--- packages/text-editor/preset-code/src/highlighter.ts
+++ packages/text-editor/preset-code/src/highlighter.ts
@@ -1,11 +1,18 @@
 //  Copyright (c) 2025 coze-dev
 //  SPDX-License-Identifier: MIT
 
-import { createHighlighter } from 'shiki';
+import { createHighlighterCore, createOnigurumaEngine } from 'shiki';
+import oneDarkPro from '@shikijs/themes/one-dark-pro';
+import githubDark from '@shikijs/themes/github-dark';
+import ts from '@shikijs/langs/ts';
+import python from '@shikijs/langs/python';
+import md from '@shikijs/langs/md';
+import js from '@shikijs/langs/js';
 
-const highlighterPromise = createHighlighter({
-  langs: ['md', 'js', 'ts', 'python'],
-  themes: ['github-dark', 'one-dark-pro'],
+const highlighterPromise = createHighlighterCore({
+  langs: [md, js, ts, python],
+  themes: [githubDark, oneDarkPro],
+  engine: createOnigurumaEngine(import('shiki/wasm')),
 });
 
 export { highlighterPromise };
