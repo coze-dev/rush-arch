8acffea3358014223fcab701b55b6d8853d10487
stream-pipe <stream-pipe@users.noreply.github.com>
2025-11-14 17:06:26 +0800

    feat: add chat preset

--- packages/text-editor/dev/src/index.tsx
+++ packages/text-editor/dev/src/index.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import Page from './pages/code';
+import Page from './pages/chat';
 import './index.css';
 
 createRoot(document.getElementById('app')!).render(<Page />);
